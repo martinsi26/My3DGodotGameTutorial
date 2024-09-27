c557d06753a3d957363565045a9c82f321629819
John Nicholson <78463373+nicholso24@users.noreply.github.com>
2024-09-27 14:05:15 -0700

    added movement func

--- Player.gd
+++ Player.gd
@@ -18,7 +18,31 @@ func _process(delta: float) -> void:
 		$Helper.idle()
 	elif(is_on_floor()):
 		$Helper.run()
+
+func default_movement(delta):
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		$Helper.jump()
+		velocity.y = JUMP_VELOCITY
 	
+	if Input.is_action_pressed("Rotate_Right"):
+		rotate_y(-0.1)
+	if Input.is_action_pressed("Rotate_Left"):
+		rotate_y(0.1)
+	
+	var input_dir := Input.get_vector("Move_Left", "Move_Right", "Move_Foward", "Move_Back")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x,0,SPEED)
+		velocity.z = move_toward(velocity.z,0,SPEED)
+	
+	move_and_slide()
 func _on_hitbox_body_entered(body: Node3D) -> void:
 	take_damage()
 
